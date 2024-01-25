be637db6114b8e8bc80790861170f8740a14f1a1
focaG <60098824+Edamatec@users.noreply.github.com>
2024-01-25 13:16:03 -0300

    Feat: Criando model do mongoose

--- sales-api/src/config/db/mongoDbConfig.js
+++ sales-api/src/config/db/mongoDbConfig.js
@@ -1,8 +1,8 @@
 import mongoose from "mongoose";
-import { MONGODB_URL } from "../secrets/secrets.js";
+import { MONGO_DB_URL } from "../secrets/secrets.js";
 
 export function connect() {
-    mongoose.connect(MONGODB_URL);
+    mongoose.connect(MONGO_DB_URL);
     mongoose.connection.on('connected', function () {
         console.log(`The application connected to mongodb successfully`);
     })
