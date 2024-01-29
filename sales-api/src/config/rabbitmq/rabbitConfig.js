da71356ed4aa820488ce2e5a8d96de1599ade8f0
focaG <60098824+Edamatec@users.noreply.github.com>
2024-01-29 14:17:32 -0300

    Feat: configurando o rabbit

--- sales-api/src/config/rabbitmq/rabbitConfig.js
+++ sales-api/src/config/rabbitmq/rabbitConfig.js
@@ -3,20 +3,35 @@ import amqp from "amqplib/callback_api.js";
 import * as secrets from "../secrets/secrets.js";
 import * as queue from "./queue.js";
 
-const HALD_SECOND = 500;
+const HALF_SECOND = 500;
+const HALF_MINUTE = 30000;
+const CONTAINER_ENV = "container";
 
 export async function connectRabbitMq() {
+    const env = process.env.NODE_ENV;
+    if (CONTAINER_ENV === env) {
+        console.log("waiting for rabbitMQ to start");
+        setInterval(() => {
+            connectRabbitMqAndCreateQueues();
+        }, HALF_MINUTE);
+    } else {
+        await connectRabbitMqAndCreateQueues();
+    }
+}
+
+async function connectRabbitMqAndCreateQueues() {
     amqp.connect(secrets.RABBIT_MQ_URL, (error, connection) => {
         if (error) {
             throw error;
         }
+        console.log("Starting rabbitMQ")
         createQueue(connection, queue.SALES_CONFIRMATION_QUEUE, queue.SALES_CONFIRMATION_ROUTING_KEY, queue.PRODUCT_TOPIC);
         createQueue(connection, queue.PRODUCT_STOCK_UPDATE_QUEUE, queue.PRODUCT_STOCK_UPDATE_ROUTING_KEY, queue.PRODUCT_TOPIC);
+        console.log("Queues and Topics were defined.")
         setTimeout(function () {
             connection.close();
-        }, HALD_SECOND);
+        }, HALF_SECOND);
     });
-
 }
 
 function createQueue(connection, queue, routingKey, topic) {
